86ddc442475bb30c3b854764e4eb7a17456f5274
Anton Levanovich <levanovich.anton@gmail.com>
2020-01-04 19:58:32 -0600

    Update Rank.js

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-    <iframe title='intro' width="560" height="315" src="https://www.youtube.com/embed/SYnh2wd3lCg" frameborder="0" allowFullScreen>
+    <iframe title='intro' width="560" height="315" src="https://www.youtube.com/embed/SYnh2wd3lCg" frameBorder="0" allowFullScreen>
     </iframe>
       <div className='white f3'>
         {`${name}, your current entry count is ...`}
